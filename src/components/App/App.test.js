34fb9de1d9c94b255e353b5915d9cf40d81e434d
Thales <thales@carteiraglobal.com>
2019-08-12 23:08:20 -0300

    Limiting Test Knowledge

---
+++ src/components/App/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+test('Should shows a comment box', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<App />, div);
+
+    /*
+        Looks inside the div
+        and checks to see if the CommentBox is in there
+    */
+    expect(div.innerHTML).toContain('CommentBox');
+
+    ReactDOM.unmountComponentAtNode(div);
+})
