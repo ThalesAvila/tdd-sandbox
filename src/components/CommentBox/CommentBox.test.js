5b329a0a1bde309a781bf8d0498d0ba2622ffa04
Thales <thales@carteiraglobal.com>
2019-09-21 15:47:50 -0300

    Asserting textarea and button

--- src/components/CommentBox/CommentBox.test.js
+++ src/components/CommentBox/CommentBox.test.js
@@ -2,8 +2,21 @@ import React from 'react';
 import {mount} from 'enzyme';
 import CommentBox from 'components/CommentBox/CommentBox'
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+})
+
+afterEach(()=> {
+  wrapped.unmount()
+})
+
 test('Should have a textarea and button', () => {
-    const wrapped = mount(<CommentBox />);
-    expect(wrapped.find('textarea').length).toEqual(1);
-    expect(wrapped.find('button').length).toEqual(1);
+  expect(wrapped.find('textarea').length).toEqual(1);
+  expect(wrapped.find('button').length).toEqual(1);
+})
+
+test('', () => {
+
 })
